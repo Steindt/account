06583f406ab3304e466cae63ba95c717dbb13a64
Tair Stenlund <tairstenen@gmail.com>
2025-02-07 03:07:16 +0100

    fixed accepting users manually

--- src/routes/create/alumni/accept/+page.server.ts
+++ src/routes/create/alumni/accept/+page.server.ts
@@ -7,22 +7,28 @@ import { sendMail } from '$lib/mail';
 export async function load({ cookies, url }) {
 	const hashed = url.searchParams.get('hash');
 	if (!hashed) throw error(400);
+	let user: User | undefined = undefined;
 	db.serialize(() => {
 		db.get(`SELECT * FROM staged WHERE hashed='${hashed}'`, (err, row) => {
-			if (err) throw error(400, 'Internal error');
-			if (!row) throw error(400, 'Account is not staged');
-			const user: User = row;
-			activateUserIPA(user);
-			console.log(`Activated an alumni account for ${user.username}`);
-			if (user.email) {
-				sendMail({
-					to: user.email,
-					subject: `Account activated`,
-					text: `Your account on dsek.se has been activated`
-				});
+			if (err) throw err;
+			if (row) {
+				user = row;
+				activateUserIPA(user);
+				console.log(`Activated an alumni account for ${user.username}`);
+			} else {
+				console.error('Account not staged');
 			}
 		});
 		db.run(`DELETE FROM staged WHERE hashed='${hashed}'`);
+		if (user && user.email) {
+			sendMail({
+				to: user.email,
+				subject: `Account activated`,
+				text: `Your account on dsek.se has been activated`
+			});
+		} else {
+			throw error(400, 'Account not staged');
+		}
 	});
 	redirect(302, '/');
 }

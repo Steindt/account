a02573445dc8d4b3d3c89affdb317bba1d428a6a
Tair Stenlund <tairstenen@gmail.com>
2025-02-07 02:28:40 +0100

    added email when accepted user

--- src/routes/create/alumni/accept/+page.server.ts
+++ src/routes/create/alumni/accept/+page.server.ts
@@ -2,6 +2,7 @@ import { error, redirect } from '@sveltejs/kit';
 import { db } from '$lib/db.js';
 import type { User } from '$lib/types.js';
 import { activateUserIPA } from '$lib/ipa.js';
+import { sendMail } from '$lib/mail';
 
 export async function load({ cookies, url }) {
 	const hashed = url.searchParams.get('hash');
@@ -9,10 +10,17 @@ export async function load({ cookies, url }) {
 	db.serialize(() => {
 		db.get(`SELECT * FROM staged WHERE hashed='${hashed}'`, (err, row) => {
 			if (err) throw error(400, 'Internal error');
-			if (!row) throw (error(400), 'Account is not staged');
+			if (!row) throw error(400, 'Account is not staged');
 			const user: User = row;
 			activateUserIPA(user);
 			console.log(`Activated an alumni account for ${user.username}`);
+			if (user.email) {
+				sendMail({
+					to: user.email,
+					subject: `Account activated`,
+					text: `Your account on dsek.se has been activated`
+				});
+			}
 		});
 		db.run(`DELETE FROM staged WHERE hashed='${hashed}'`);
 	});
